761de36b70a03e2fa35ae5637900cf1fb3d08793
Kenneth Maleitzke <ken.maleitzke@gmail.com>
2019-02-17 10:53:07 -0500

    Add project files.

---
+++ WPF_MasterDetailApp.S1.Str/BusinessLayer/ProductBL.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WPF_MasterDetailApp;
+using WPF_MasterDetailApp.Models;
+
+namespace WPF_MasterDetailApp.BusinessLayer
+{
+    public class ProductBL
+    {
+        #region ENUMS
+
+
+
+        #endregion
+
+        #region FIELDS
+
+        ProductWindowView _productWindowView;
+        ProductWindowPresenter _productWindowPresenter;
+
+        #endregion
+
+        #region PROPERTIES
+
+        #endregion
+
+        #region CONSTRUCTORS
+        public ProductBL() {
+            //
+            // instantiate the view model and initialize the data set
+            //
+            _productWindowPresenter = new ProductWindowPresenter(GetComanptData(), GetProductData());
+
+            //
+            // instantiate, set the data context, and show the Main Window
+            //
+            _productWindowView = new ProductWindowView(_productWindowPresenter);
+            _productWindowView.DataContext = _productWindowPresenter;
+            _productWindowView.Show();
+        }
+
+        private Product GetProductData()
+        {
+            return new Product()
+            {
+                Id = 0,
+                TypeOfMustard = "Yellow Mustard",
+                CompanyName = "Heinz",
+                Age = 35,
+                Gender = Product.GenderType.male,
+                PriceOfProduct = 1.00,
+                HireDate = DateTime.Parse("7/7/7"),
+                Description = "The most common type of mustard.",
+                ImageFileName = "mustard.jpg"
+            };
+        }
+
+        private Company GetComanptData()
+        {
+            Company company = new Company();
+
+            company.Name = "Mustard Distribution Agency";
+            company.Address = "11 Front Street";
+            company.City = "Traverse City";
+
+            return company;
+        }
+        #endregion
+
+        #region METHODS
+
+
+
+        #endregion
+
+        #region EVENTS
+
+
+        #endregion
+
+    }
+}
---
+++ WPF_MasterDetailApp.S1.Str/Models/Product.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MasterDetailApp.Models
+{
+    public class Product
+    {
+        #region ENUMS
+
+        public enum GenderType { none, male, female }
+
+        #endregion
+
+        #region FIELDS
+
+        private int _id;
+        private string _companyName;
+        private string _typeOfMustard;
+        private int _age;
+        private GenderType _gender;
+        private string _imageFileName;
+        private string _description;
+        private DateTime _hireDate;
+        private double _priceOfProduct;
+
+        #endregion
+
+        #region PROPERTIES
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string CompanyName
+        {
+            get { return _companyName; }
+            set
+            {
+                _companyName = value;
+            }
+        }
+
+        public string TypeOfMustard
+        {
+            get { return _typeOfMustard; }
+            set
+            {
+                _typeOfMustard = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public GenderType Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+
+        public string ImageFileName
+        {
+            get { return _imageFileName; }
+            set { _imageFileName = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public DateTime HireDate
+        {
+            get { return _hireDate; }
+            set { _hireDate = value; }
+        }
+
+        public double PriceOfProduct
+        {
+            get { return _priceOfProduct; }
+            set { _priceOfProduct = value; }
+        }
+
+        public string FullName
+        {
+            get { return _companyName + " " + _typeOfMustard; }
+        }
+
+        public string ImageFilePath
+        {
+            get { return @"../Images/" + _imageFileName; }
+        }
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public Product()
+        {
+
+        }
+
+        #endregion
+
+        #region METHODS
+
+
+        #endregion
+
+        #region EVENTS
+
+
+
+        #endregion
+    }
+}
